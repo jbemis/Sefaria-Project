ac7a758df7682d8e05e4bcfc0ec864e1adda1f72
Ephraim Damboritz <ephraim@sefaria.org>
2016-12-01 16:59:56 +0200

    bug fix in a migration script

--- scripts/commentary-refactor-scripts/fix_misc_indices.py
+++ scripts/commentary-refactor-scripts/fix_misc_indices.py
@@ -4,10 +4,10 @@ from sefaria.model import *
 
 rifs = IndexSet({"title": {"$regex": "^Rif"}})
 for idx in rifs:
+    print idx.title
     idx.categories.insert(1, "Commentary")
-    idx.save()
-    base_book = idx.title.replace(work_title + " ", '').strip()
-    idx.dependence = 'commentary'
+    base_book = idx.title.replace("Rif ", '').strip()
+    idx.dependence = 'Commentary'
     idx.work_title = "Rif"
     idx.base_text_mapping = None
     bidx = library.get_index(base_book)
